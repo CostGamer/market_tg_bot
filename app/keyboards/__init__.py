2505d8d31548b6c9f51bcece01f2d4b647378461
VB <vladimirbryzgalov00@gmail.com>
2025-06-15 03:24:38 +0700

    change keyboard to inline

--- app/keyboards/__init__.py
+++ app/keyboards/__init__.py
@@ -1,5 +1,5 @@
-from .categories import get_categories_keyboard
+from .categories import get_categories_inline_keyboard
 
 __all__ = [
-    "get_categories_keyboard",
+    "get_categories_inline_keyboard",
 ]
